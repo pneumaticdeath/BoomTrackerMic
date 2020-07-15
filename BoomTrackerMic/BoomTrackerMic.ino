348d8303da44d0e06a0050dfc0fc33b98c2d7b80
Mitch Patenaude <patenaude@gmail.com>
2020-07-15 12:53:44 -0700

    Beginnings of ESP32 framework, switched M0 to use internal timer

--- BoomTrackerMic/BoomTrackerMic.ino
+++ BoomTrackerMic/BoomTrackerMic.ino
@@ -1,9 +1,11 @@
 #define SERIAL_DEBUG
 #define READ_IN_ISR
-#ifndef ESP8266
-#define MIC_ID "001"
+#if defined(ESP8266)
+# define MIC_ID "002"
+#elif defined(ESP32)
+# define MIC_ID "003"
 #else
-#define MIC_ID "002"
+# define MIC_ID "001"
 #endif
 
 #if defined(ESP8266)
@@ -17,14 +19,32 @@
 # include <DHT_U.h>
 # include <ESP8266WiFi.h>
 
+
+#elif defined(ESP32)
+//???
+#include <WiFi.h>
+#include <AsyncUDP.h>
+
+#include <ESP32TimerInterrupt.h>
+
 #else // assume Feather M0 WiFi
 
 # include <SPI.h>
 # include <WiFi101.h>
 # include "RTClib.h"
 # define USE_RTC 1
-#define ERROR_LED_PIN 13
+# define USE_M0_INTERNAL_TIMER
+# define ERROR_LED_PIN 13
 
+# if defined(USE_M0_INTERNAL_TIMER)
+/*
+ * Taken from https://gist.github.com/jdneo/43be30d85080b175cb5aed3500d3f989
+ */
+
+void startTimer(int frequencyHz);
+void setTimerFrequency(int frequencyHz);
+void TC3_Handler();
+# endif
 #endif
 
 #include <WiFiUdp.h>
@@ -45,6 +65,11 @@ volatile uint32_t epochMicros = 0;
 // Init ESP8266 timer 0
 ESP8266Timer ITimer;
 # define CLOCK_RATE 400
+#elif defined(ESP32)
+// TODO
+#elif defined(USE_M0_INTERNAL_TIMER)
+# define CLOCK_RATE 10000
+RTC_DS3231 rtc;
 #else
 const uint8_t clockPin = 10; // the pin receiving the clock signal.
 # define CLOCK_RATE 8192
@@ -210,7 +235,7 @@ void setup() {
   timeClient.begin();
   timeClient.setUpdateInterval(1000); // 1 second
 
-#ifndef ESP8266
+#if !defined(ESP8266)
   analogReadResolution(12);
 #endif
 
@@ -233,9 +258,13 @@ void setup() {
   //}
   initializeRtcFromNtp();
 
+# if defined(USE_M0_INTERNAL_TIMER)
+  startTimer(CLOCK_RATE);
+# else
   pinMode(clockPin, INPUT_PULLUP);
   rtc.writeSqwPinMode(DS3231_SquareWave8kHz);
   attachInterrupt(digitalPinToInterrupt(clockPin), clock_tick, FALLING);
+# endif
 #endif
 
 #if defined(ESP8266)
@@ -533,3 +562,64 @@ void refreshNTP() {
     LOG_ERROR("Time update failed");
   }
 }
+
+#if defined(USE_M0_INTERNAL_TIMER)
+
+#define CPU_HZ 48000000
+#define TIMER_PRESCALER_DIV 1024
+
+void setTimerFrequency(int frequencyHz) {
+  int compareValue = (CPU_HZ / (TIMER_PRESCALER_DIV * frequencyHz)) - 1;
+  TcCount16* TC = (TcCount16*) TC3;
+  // Make sure the count is in a proportional position to where it was
+  // to prevent any jitter or disconnect when changing the compare value.
+  TC->COUNT.reg = map(TC->COUNT.reg, 0, TC->CC[0].reg, 0, compareValue);
+  TC->CC[0].reg = compareValue;
+  Serial.println(TC->COUNT.reg);
+  Serial.println(TC->CC[0].reg);
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+}
+
+void startTimer(int frequencyHz) {
+  REG_GCLK_CLKCTRL = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TCC2_TC3) ;
+  while ( GCLK->STATUS.bit.SYNCBUSY == 1 ); // wait for sync
+
+  TcCount16* TC = (TcCount16*) TC3;
+
+  TC->CTRLA.reg &= ~TC_CTRLA_ENABLE;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  // Use the 16-bit timer
+  TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  // Use match mode so that the timer counter resets when the count matches the compare register
+  TC->CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  // Set prescaler to 1024
+  TC->CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1024;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  setTimerFrequency(frequencyHz);
+
+  // Enable the compare interrupt
+  TC->INTENSET.reg = 0;
+  TC->INTENSET.bit.MC0 = 1;
+
+  NVIC_EnableIRQ(TC3_IRQn);
+
+  TC->CTRLA.reg |= TC_CTRLA_ENABLE;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+}
+
+void TC3_Handler() {
+  TcCount16* TC = (TcCount16*) TC3;
+  // If this interrupt is due to the compare register matching the timer count
+  // we call the clock_tick() function
+  if (TC->INTFLAG.bit.MC0 == 1) {
+    TC->INTFLAG.bit.MC0 = 1;
+    clock_tick();
+  }
+}
+#endif
